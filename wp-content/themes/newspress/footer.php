15fd24340da66948b9866b511fe4407cf5f4ca98
Gonzalo Bellver <gonzalo.bellver@gmail.com>
2012-02-08 15:27:59 -0300

    Footer

--- wp-content/themes/newspress/footer.php
+++ wp-content/themes/newspress/footer.php
@@ -1,3 +1,76 @@
 <div id="footer" class="clearfix">
-    <h2>Hola</h2>
+
+    <div id="footer-quienes-somos" class="footer-column">
+        <h2>¿Quiénes somos?</h2>
+        <p>Somos una asociación civil sin fines de lucro surgida en 2008 cuya misión es promover un cambio de la comunidad en su relación con el medio ambiente a partir de la difusión de los valores consumo responsable y responsabilidad conjunta. Apuntamos a:</p>
+        <ul>
+            <li>Lo que apuntamos 1</li>
+            <li>Lo que apuntamos 2</li>
+        </ul>
+        <div class="color-line"></div>
+    </div>
+
+    <div id="footer-colaborators" class="footer-column">
+        <h2>Colaboradores</h2>
+        <ul>
+            <li>Colaborador 1</li>
+            <li>Colaborador 2</li>
+            <li>Colaborador 3</li>
+            <li>Colaborador 4</li>
+            <li>Colaborador 5</li>
+        </ul>
+        <div class="color-line"></div>
+    </div>
+
+    <div id="footer-contact" class="footer-column">
+        <h2>Contacto</h2>
+        <ul>
+            <li>Contact 1</li>
+            <li>Contact 2</li>
+            <li>Contact 3</li>
+        </ul>
+        <div class="color-line"></div>
+    </div>
+
+    <div id="friends" class="clearfix" style="padding: 20px">
+        <h2>Amigos</h2>
+
+        <a class="friend-link" href="http://www.greenfilmfest.com.ar/">
+            <img src="<?php bloginfo('template_directory');?>/library/images/amigos/gff.jpg"/>
+        </a>
+
+        <a class="friend-link" href="http://lavidaenbici.com/">
+            <img src="<?php bloginfo('template_directory');?>/library/images/amigos/la-vida-en-bici.jpg"/>
+        </a>
+
+        <a class="friend-link" href="http://www.grecaweb.com/">
+            <img src="<?php bloginfo('template_directory');?>/library/images/amigos/greca.jpg"/>
+        </a>
+
+        <a class="friend-link" href="http://sustentator.com/">
+            <img src="<?php bloginfo('template_directory');?>/library/images/amigos/sustentator.jpg"/>
+        </a>
+
+        <a class="friend-link" href="http://www.revistaplot.com/">
+            <img src="<?php bloginfo('template_directory');?>/library/images/amigos/plot.jpg"/>
+        </a>
+
+        <a class="friend-link" href="http://www.britishcouncil.org/es/argentina.htm">
+            <img src="<?php bloginfo('template_directory');?>/library/images/amigos/british-council.jpg"/>
+        </a>
+
+        <a class="friend-link" href="http://www.dqbstudio.com/">
+            <img src="<?php bloginfo('template_directory');?>/library/images/amigos/dqb.jpg"/>
+        </a>
+
+        <a class="friend-link" href="http://www.ehma.com.ar/">
+            <img src="<?php bloginfo('template_directory');?>/library/images/amigos/ehma.jpg"/>
+        </a>
+
+        <a class="friend-link" href="http://www.sabelatierra.com/">
+            <img src="<?php bloginfo('template_directory');?>/library/images/amigos/sabe-la-tierra.jpg"/>
+        </a>
+
+
+    </div>
 </div>
