ce9fecf7564709dda57635ff07c434a6285583eb
Gonzalo Bellver <gonzalo.bellver@gmail.com>
2012-02-09 12:55:32 -0300

    Thumbnailing

--- wp-content/themes/newspress/functions.php
+++ wp-content/themes/newspress/functions.php
@@ -702,6 +702,20 @@ function build_admin_head() {
 		<?php
 	}  //end of theme accesibility mode
 }
+
+function catch_that_image() {
+    global $post, $posts;
+    $first_img = '';
+    ob_start();
+    ob_end_clean();
+    $output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $post->post_content, $matches);
+    $first_img = $matches [1] [0];
+
+    if(empty($first_img)){ //Defines a default image
+        $first_img = "/images/default.jpg";
+    }
+    return $first_img;
+}
 	
 add_action('admin_menu', 'startit');
 add_action('admin_head', 'build_admin_head');
